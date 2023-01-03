69c30fb8a710d62422ce0d7d8a855bc73e6829e2
raiyuvraj <yuvirai017@gmail.com>
2023-01-03 04:52:49 -0800

    [Yuvraj]-WhileLoop

--- DailyPractice/Program.cs
+++ DailyPractice/Program.cs
@@ -4,9 +4,16 @@ class forloop
 {
     public static void Main()
     {
-        for(int i = 1; i <= 10; i++) 
+        //for(int i = 1; i <= 10; i++) 
+        //{
+        //    Console.WriteLine(i);
+        //}
+        int i = 1;
+
+        while(i < 10)
         {
-            Console.WriteLine(i);
+            Console.WriteLine("Yuvraj");
         }
+        i++;
     }
 }
